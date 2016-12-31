b18cd258b3c3c70b0f177bdea59c1f2bbc9dd7c0
Piotr Sawczuk <piotr.sawczuk0@gmail.com>
2016-12-31 20:08:52 +0100

    adding previously written application

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            new ZipBomb(i).run();
+        }
     }
 }
---
+++ src/ZipBomb.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ZipBomb implements Runnable{
+    private final File file;
+    private final int number;
+
+    public ZipBomb(int number) {
+        this.number = number;
+        file = new File(System.getProperty("user.dir") + "/bomb" + number + ".bin");
+    }
+
+    private void newFile() {
+        FileOutputStream fileOutputStream = null;
+        Random generator = new Random();
+        byte[] data = new byte[1024 * 1024];
+        generator.nextBytes(data);
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(data, 0, data.length);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ZipBomb.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+        }
+    }
+
+    @Override
+    public void run() {
+        newFile();
+        System.out.println("Thread " + number + " has finished.");
+    }
+}
